23be7c913d5cde1a6c6af83a0646838c2f5ad3a0
Pillows <matthew.wong@outlook.com>
2014-08-26 19:46:49 -0400

    New directory lister

--- index.php
+++ index.php
@@ -1,77 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-   <link rel="stylesheet" href="bootstrap-responsive.min.css">
-   <link rel="stylesheet" href="bootstrap.min.css">
-   
-   <title>Display Directory Contents</title>
-   <style>
-     *{
-         padding:0;
-         margin:0;
-     }
+<?php
 
-	#container{
-		padding:30px;
-	}
-  
+    // Include the DirectoryLister class
+    require_once('resources/DirectoryLister.php');
 
-   </style>
+    // Initialize the DirectoryLister object
+    $lister = new DirectoryLister();
 
-</head>
-<body>
+    // Return file hash
+    if (isset($_GET['hash'])) {
 
-   <div id="container">
-       <?php
-         // opens this directory
-         $myDirectory = opendir("./scripts");
+        // Get file hash array and JSON encode it
+        $hashes = $lister->getFileHash($_GET['hash']);
+        $data   = json_encode($hashes);
 
-         // gets each entry
-         while($entryName = readdir($myDirectory)) {
-           $dirArray[] = $entryName;
-         }
+        // Return the data
+        die($data);
 
-         // finds extention of file
-         function findexts ($filename)
-         {
-           $filename = strtolower($filename) ;
-           $exts = split("[/\\.]", $filename) ;
-           $n = count($exts)-1;
-           $exts = $exts[$n];
-           return strtoupper($exts);
-         }
+    }
 
-         // closes directory
-         closedir($myDirectory);
+    // Initialize the directory array
+    if (isset($_GET['dir'])) {
+        $dirArray = $lister->listDirectory($_GET['dir']);
+    } else {
+        $dirArray = $lister->listDirectory('.');
+    }
 
-         //  counts elements in array
-         $indexCount   = count($dirArray);
+    // Define theme path
+    if (!defined('THEMEPATH')) {
+        define('THEMEPATH', $lister->getThemePath());
+    }
 
-         // sorts files
-         sort($dirArray);
+    // Set path to theme index
+    $themeIndex = $lister->getThemePath(true) . '/index.php';
 
-         // print 'em
-         print("<h1>Directory Contents</h1>");
-         print("<table width='100%' cellspacing='10' class='table'>
-                 <tr>
-                   <td class='head'>Filename</td>
-                   <td class='head'>Type</td>
-                   <td class='head'>Size</td></tr>\n");
-
-         // loops through the array of files and print them all
-         for($index=0; $index < $indexCount; $index++) {
-               if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
-					if (findexts($dirArray[$index]) == strtoupper($dirArray[$index])) {
-						echo "<tr><td><a href='$dirArray[$index]'>$dirArray[$index]/</a></td>"."<td> Folder </td>"."<td>".round(filesize($dirArray[$index])/1000,2)." Mb"."</td>"."</tr>\n";
-					}else{
-						echo "<tr><td><a href='$dirArray[$index]'>$dirArray[$index]</a></td>"."<td>".findexts($dirArray[$index])."</td>"."<td>".round(filesize($dirArray[$index])/1000,2)." Mb"."</td>"."</tr>\n";
-					}
-           	   }
-         }
-         echo "</table>\n";
-       ?>
-   </div>
-
-</body>
-</html>
+    // Initialize the theme
+    if (file_exists($themeIndex)) {
+        include($themeIndex);
+    } else {
+        die('ERROR: Failed to initialize theme');
+    }
